7cdf989f6817b6695fe1584375dd4440617a7809
AntiqueR <antique.pendulum@gmail.com>
2020-08-04 18:03:34 +0900

    init

---
+++ P2PTester/Client/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2PTester.Client
+{
+    class Client
+    {
+    }
+}
--- P2PTester/Program.cs
+++ P2PTester/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
 
 namespace P2PTester
 {
